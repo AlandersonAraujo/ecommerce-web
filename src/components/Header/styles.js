f6bae9e96f8a2562276184d92a2a4dfceb2fbf26
AlandersonAraujo <alandersoncaraujo@gmail.com>
2019-09-17 23:41:05 -0300

    Header

---
+++ src/components/Header/styles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const Container = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 50px 0;
+`;
+
+export const Cart = styled(Link)`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  transition: opacity 0.2s;
+
+  &:hover {
+    opacity: 0.7;
+  }
+
+  div {
+    text-align: right;
+    margin-right: 10px;
+
+    strong {
+      display: block;
+      color: #f5f5f5;
+    }
+
+    span {
+      font-size: 13px;
+      color: #f3f3f3;
+    }
+  }
+`;
